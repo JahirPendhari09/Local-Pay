7186401281660f187fd1361add8609d78584c647
JahirPendhari <jahirpp1999@gmail.com>
2023-08-27 11:18:37 +0530

    render Attached

--- local-pay/src/Pages/DashBoard.jsx
+++ local-pay/src/Pages/DashBoard.jsx
@@ -1,57 +1,67 @@
 import { Box,Grid,Center, HStack ,Image,Text, VStack,Button,useMediaQuery} from "@chakra-ui/react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { PaymentSection } from "./PaymentSection"
 import style from "../Styles/payment.module.css"
 import { Link } from "react-router-dom"
-
+import axios from "axios"
 
 const bgColor=["lightgreen", "pink", "lightblue", "orange","lightgray"]
-const history=[
-    {
-        id:0,
-        firstName:"John",
-        lastName: "Doe",
-        location:"Sent",
-        money:"$310",
-        date:"14 Jan 2023"
-    },
-    {
-        id:1,
-        firstName:"Jahir",
-        lastName: "Pendhari",
-        location:"Received",
-        money:"$1000",
-        date:"15 Feb 2023"
-    },
-    {
-        id:2,
-        firstName:"Akshay",
-        lastName: "Patil",
-        location:"Exchange",
-        money:"$100",
-        date:"21 Feb 2023"
-    },
-    {
-        id:4,
-        firstName:"TATA",
-        lastName: "Balanced",
-        location:"Mutual Funds",
-        money:"$500",
-        date:"25 Feb 2023"
-    },
-     {
-        id:5,
-        firstName:"Salman",
-        lastName: "Khan",
-        location:"Recieved",
-        money:"$150",
-        date:"25 Feb 2023"
-    }
-]
+// const history=[
+//     {
+//         id:0,
+//         firstName:"John",
+//         lastName: "Doe",
+//         location:"Sent",
+//         money:"$310",
+//         date:"14 Jan 2023"
+//     },
+//     {
+//         id:1,
+//         firstName:"Jahir",
+//         lastName: "Pendhari",
+//         location:"Received",
+//         money:"$1000",
+//         date:"15 Feb 2023"
+//     },
+//     {
+//         id:2,
+//         firstName:"Akshay",
+//         lastName: "Patil",
+//         location:"Exchange",
+//         money:"$100",
+//         date:"21 Feb 2023"
+//     },
+//     {
+//         id:4,
+//         firstName:"TATA",
+//         lastName: "Balanced",
+//         location:"Mutual Funds",
+//         money:"$500",
+//         date:"25 Feb 2023"
+//     },
+//      {
+//         id:5,
+//         firstName:"Salman",
+//         lastName: "Khan",
+//         location:"Recieved",
+//         money:"$150",
+//         date:"25 Feb 2023"
+//     }
+// ]
 const DashBoard =()=>{
     const [extraPaymentSection, setExtraPaymentSection]= useState(false)
     const [isLargeScreen] = useMediaQuery("(max-width: 768px)");
 
+    const [transaction_History, setHistory]= useState([]);
+
+    const fetchData =()=>{
+          axios.get("https://local-pay.onrender.com/transaction_History")
+        .then(res=> setHistory(res.data));
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+    console.log(transaction_History)
     
 
     return <>
@@ -102,9 +112,9 @@ const DashBoard =()=>{
           <Text fontSize="20px"color="blue"> <Link >View All</Link></Text>
       </HStack>
       <Grid templateColumns={['1fr', '1fr', '1fr 1fr']}gap="15px" marginTop={4}>
-      {history.map((item,i)=>{
-        return <HStack id={item.id} justifyContent="space-around"w="60%" margin="auto" border="1px" borderRadius="20px"  p={4} fontSize="20px"bg="bisque" marginBottom="20px" gap={4}>
-            <Text border="1px solid black" marginLeft={0} borderRadius="50%" w="60px" h="60px" align="center" paddingTop="15px" bg={bgColor[i%bgColor.length]}>{item.firstName.charAt(0).toUpperCase()+item.lastName.charAt(0).toUpperCase()}</Text>
+      {transaction_History && transaction_History?.length>0 && transaction_History.map((item,i)=>{
+        return <HStack key={item.id} justifyContent="space-around"w="60%" margin="auto" border="1px" borderRadius="20px"  p={4} fontSize="20px"bg="bisque" marginBottom="20px" gap={4}>
+            <Text border="1px solid black" marginLeft={0} borderRadius="50%" w="60px" h="60px" align="center" paddingTop="15px" bg={bgColor[i%bgColor.length]}>{`${item.firstName[0]}${item.lastName[0]}`}</Text>
             <VStack>
                 <Text color="blue.400">{item.firstName+" "+ item.lastName}</Text>
                 <Text color={item.location=="Sent"? "red":"green"}>{item.location}</Text>
